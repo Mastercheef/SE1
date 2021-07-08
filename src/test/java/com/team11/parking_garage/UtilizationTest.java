af2fbd687e1237b880d52773cf1773d1017b047b
Marian Hönscheid <admin@marianhoenscheid.de>
2021-07-09 00:01:07 +0200

    fix duplications in UtilizationTest

--- src/test/java/com/team11/parking_garage/UtilizationTest.java
+++ src/test/java/com/team11/parking_garage/UtilizationTest.java
@@ -37,11 +37,11 @@ class UtilizationTest {
         String t4 = t + "59000";
         String t5 = t + "60000";
 
-        Car c1 = new Car(new String[]{ENTER,"1",t1,"_","_","78477000d3f4486557edd3245febeec0","#03b428","14", STUDENT,"Kombi","SU-B 26"}, new Standard(1));
-        Car c2 = new Car(new String[]{ENTER,"2",t2,"_","_","f55fb9cbe110192e12c21dc734b6511c","#69524f","4", STUDENT,"SUV","SU-G 93"}, new Subscriber(2));
-        Car c3 = new Car(new String[]{ENTER,"3",t3,"_","_","c8ed72e96795b1970367a5d058457ed9","#2a3e73","1","Familie","Limousine","SU-A 14"}, new Discounted(3, STUDENT));
-        Car c4 = new Car(new String[]{ENTER,"4",t4,"_","_","c354d2014d1c63c9be162400be104894","#d6a3ea","18","Standard","SUV","SU-J 99"}, new Discounted(4,"Senior"));
-        Car c5 = new Car(new String[]{ENTER,"5",t5,"_","_","d2227edd8ae39c18375039e14602afb4","#6c5d42","12", STUDENT,"SUV","SU-Q 66"}, new Discounted(5, "Familie"));
+        Car c1 = new Car(new String[]{ENTER,"1",t1,"_","_","36fb95b76d5a5315cb501ca5abadaa53","#8d9171","14", STUDENT,"Kombi","SU-B 26"}, new Standard(1));
+        Car c2 = new Car(new String[]{ENTER,"2",t2,"_","_","0678fd574b14db79f35a5b8dff7f24ed","#bda477","4", STUDENT,"SUV","SU-G 93"}, new Subscriber(2));
+        Car c3 = new Car(new String[]{ENTER,"3",t3,"_","_","517df966c5e5b280f18585efa411601e","#8aaa2f","1","Familie","Limousine","SU-A 14"}, new Discounted(3, STUDENT));
+        Car c4 = new Car(new String[]{ENTER,"4",t4,"_","_","81607fe4e3b6fede95ab8d8ec99b511e","#c4d291","18","Standard","SUV","SU-J 99"}, new Discounted(4,"Senior"));
+        Car c5 = new Car(new String[]{ENTER,"5",t5,"_","_","5ef7f4d31c1b37808dbfe800d29b76af","#f39ca9","12", STUDENT,"SUV","SU-Q 66"}, new Discounted(5, "Familie"));
 
         utilizationTestList = new ArrayList<>();
         utilizationList = new ArrayList<>();
