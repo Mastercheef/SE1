1f606aab270a278255e5070eaa9d7fa5c105000e
Marian Hönscheid <admin@marianhoenscheid.de>
2021-07-09 00:10:29 +0200

    fix duplications in UtilizationTest

--- src/test/java/com/team11/parking_garage/UtilizationTest.java
+++ src/test/java/com/team11/parking_garage/UtilizationTest.java
@@ -37,33 +37,27 @@ class UtilizationTest {
         String t4 = t + "59000";
         String t5 = t + "60000";
 
-        Car c1 = new Car(new String[]{ENTER,"1",t1,"_","_","36fb95b76d5a5315cb501ca5abadaa53","#8d9171","13", STUDENT,"SUV","SU-U 75"}, new Standard(1));
-        Car c2 = new Car(new String[]{ENTER,"2",t2,"_","_","0678fd574b14db79f35a5b8dff7f24ed","#bda477","5", STUDENT,"Kombi","SU-Y 4"}, new Subscriber(2));
-        Car c3 = new Car(new String[]{ENTER,"3",t3,"_","_","517df966c5e5b280f18585efa411601e","#8aaa2f","2","Familie","SUV","SU-B 74"}, new Discounted(3, STUDENT));
-        Car c4 = new Car(new String[]{ENTER,"4",t4,"_","_","81607fe4e3b6fede95ab8d8ec99b511e","#c4d291","19","Standard","SUV","SU-N 38"}, new Discounted(4,"Familie"));
-        Car c5 = new Car(new String[]{ENTER,"5",t5,"_","_","5ef7f4d31c1b37808dbfe800d29b76af","#f39ca9","20", STUDENT,"Limousine","SU-Y 76"}, new Discounted(5, "Senior"));
-
         utilizationTestList = new ArrayList<>();
         utilizationList = new ArrayList<>();
         utilization = Utilization.getInstance();
 
-        cars.add(c1);
+        cars.add(new Car(new String[]{ENTER,"1",t1,"_","_","36fb95b76d5a5315cb501ca5abadaa53","#8d9171","13", STUDENT,"SUV","SU-U 75"}, new Standard(1)));
         unixtimestamp = String.valueOf(Instant.now().toEpochMilli());
         utilizationList = utilization.getUtilizationNow(utilizationList, cars, context);
         utilizationTestList.add(new String[]{unixtimestamp, "10"});
-        cars.add(c2);
+        cars.add(new Car(new String[]{ENTER,"2",t2,"_","_","0678fd574b14db79f35a5b8dff7f24ed","#bda477","5", STUDENT,"Kombi","SU-Y 4"}, new Subscriber(2)));
         unixtimestamp = String.valueOf(Instant.now().toEpochMilli());
         utilizationList = utilization.getUtilizationNow(utilizationList, cars, context);
         utilizationTestList.add(new String[]{unixtimestamp, "20"});
-        cars.add(c3);
+        cars.add(new Car(new String[]{ENTER,"3",t3,"_","_","517df966c5e5b280f18585efa411601e","#8aaa2f","2","Familie","SUV","SU-B 74"}, new Discounted(3, STUDENT)));
         unixtimestamp = String.valueOf(Instant.now().toEpochMilli());
         utilizationList = utilization.getUtilizationNow(utilizationList, cars, context);
         utilizationTestList.add(new String[]{unixtimestamp, "30"});
-        cars.add(c4);
+        cars.add(new Car(new String[]{ENTER,"4",t4,"_","_","81607fe4e3b6fede95ab8d8ec99b511e","#c4d291","19","Standard","SUV","SU-N 38"}, new Discounted(4,"Familie")));
         unixtimestamp = String.valueOf(Instant.now().toEpochMilli());
         utilizationList = utilization.getUtilizationNow(utilizationList, cars, context);
         utilizationTestList.add(new String[]{unixtimestamp, "40"});
-        cars.add(c5);
+        cars.add(new Car(new String[]{ENTER,"5",t5,"_","_","5ef7f4d31c1b37808dbfe800d29b76af","#f39ca9","20", STUDENT,"Limousine","SU-Y 76"}, new Discounted(5, "Senior")));
         unixtimestamp = String.valueOf(Instant.now().toEpochMilli());
         utilizationList = utilization.getUtilizationNow(utilizationList, cars, context);
         utilizationTestList.add(new String[]{unixtimestamp, "50"});
