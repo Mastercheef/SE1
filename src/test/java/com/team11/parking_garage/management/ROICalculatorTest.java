f38a8902cbdac8fb8392cc4c6a39647c8250b1ac
Marian Hönscheid <admin@marianhoenscheid.de>
2021-07-08 19:01:08 +0200

    added more tests

---
+++ src/test/java/com/team11/parking_garage/management/ROICalculatorTest.java
@@ -0,0 +1,34 @@
+package com.team11.parking_garage.management;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ROICalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+        /* TODO */
+    }
+
+    @Test
+    void returnInvest() {
+        /* TODO */
+    }
+
+    @Test
+    void amortisationMonths() {
+        /* TODO */
+    }
+
+    @Test
+    void amortisationYears() {
+        /* TODO */
+    }
+
+    @Test
+    void getAsJson() {
+        /* TODO */
+    }
+}
